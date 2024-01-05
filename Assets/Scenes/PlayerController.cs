9e69d73a750e7be7b4b915fd72f9a6cf27bb8938
min4034415 <65104668+min4034415@users.noreply.github.com>
2024-01-05 19:15:33 +0900

    Testing

---
+++ Assets/Scenes/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+    void Start(){
+
+    }
+    void Update() {
+        
+    }
+
+}

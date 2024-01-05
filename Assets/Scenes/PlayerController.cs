87d8d1d1739636a18e6ebb9671887dfd13190e91
min4034415 <65104668+min4034415@users.noreply.github.com>
2024-01-05 19:46:04 +0900

    Kinda Fun isn't it

--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -10,7 +10,26 @@ public class PlayerController : MonoBehaviour
 
     }
     void Update() {
-        
+        if (Input.GetKey(KeyCode.UpArrow) == true){
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true){
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true){
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true){
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
+
+    }
+
+    public void Die() {
+        gameObject.SetActive(false);
     }
 
 }

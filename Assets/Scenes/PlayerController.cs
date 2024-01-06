82f08ce767fb7627ab8924355b2e6b00094838b7
min4034415 <65104668+min4034415@users.noreply.github.com>
2024-01-06 10:08:44 +0900

    Some changes that puts wasd to command control

--- Assets/Scenes/PlayerController.cs
+++ Assets/Scenes/PlayerController.cs
@@ -10,22 +10,14 @@ public class PlayerController : MonoBehaviour
         playerRigidbody = GetComponent<Rigidbody>();
     }
     void Update() {
-        if (Input.GetKey(KeyCode.UpArrow) == true){
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.DownArrow) == true){
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
 
-        if (Input.GetKey(KeyCode.RightArrow) == true){
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow) == true){
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
-
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
     }
 
     public void Die() {

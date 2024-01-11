4d181d353fa5324a5fa0080ac6c120249859063c
min4034415 <65104668+min4034415@users.noreply.github.com>
2024-01-11 14:51:34 +0900

    Take me Back

--- Assets/Scenes/Scripts/GameManager.cs
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameover;
+
     void Start()
     {
+        surviveTime = 0;
+        isGameover = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isGameover)
+        {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time: " + (int)surviveTime;
+        }
+    }
+
+    public void EndGame() {
         
     }
 }
